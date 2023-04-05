89cec28f7748e71a61c9efc67bb37334347ecac5
estong jamion <estong.jamion@gmail.com>
2023-04-05 21:45:47 +0300

    Added button style as component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
 import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1>Meals for life</h1>
-        <button type="button">Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="A table full of delicious foods!" />
