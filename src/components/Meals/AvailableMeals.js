34f13172e85ee014524dacec0a24ed19c6567494
estong jamion <estong.jamion@gmail.com>
2023-04-05 22:16:27 +0300

    Added meals summary and meals item components and card ui components

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,54 @@
+import React from "react";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem";
+
+const DUMMY_MEALS = [
+  {
+    id: 1,
+    name: "Spaghetti Bolognese",
+    description: "Spaghetti with a rich meat sauce",
+    price: 12.99,
+  },
+  {
+    id: 2,
+    name: "Chicken Caesar Salad",
+    description:
+      "Grilled chicken with romaine lettuce, croutons, and Caesar dressing",
+    price: 8.99,
+  },
+  {
+    id: 3,
+    name: "Beef Burger",
+    description: "Grilled beef patty with lettuce, tomato, and cheese on a bun",
+    price: 10.99,
+  },
+  {
+    id: 4,
+    name: "Vegetable Curry",
+    description: "A spicy vegetarian curry with chickpeas and potatoes",
+    price: 9.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => {
+    return (
+      <MealItem
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
+  });
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
